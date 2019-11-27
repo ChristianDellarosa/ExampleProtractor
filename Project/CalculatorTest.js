4840ecdb59aa9a80e8d44c95cc6d8e929ffdb3a0
Christian Dellarosa <CDellarosa@raetlatam.com>
2019-11-27 20:27:06 -0300

    Create CalculatorTest

---
+++ Project/CalculatorTest.js
@@ -0,0 +1,47 @@
+
+describe('Protractor Example', function() {
+  var URL = "http://juliemr.github.io/protractor-demo/";
+  browser.get(URL);
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var acceptButton = element(by.id('gobutton'));
+  var listOperators = element.all(by.model('operator'));
+  var divisionOperator = listOperators.all(by.xpath("//option[@value='DIVISION']"));
+
+
+  function input(number1, number2, operator) {
+    firstNumber.sendKeys(number1);
+    secondNumber.sendKeys(number2);
+    listOperators.click();
+    listOperators.all(by.xpath("//option[@value='"+operator+"']")).click();
+    acceptButton.click();
+    switch (operator) {
+      case "DIVISION":
+          return number1/number2;      
+          break;
+      case "ADDITION":
+        return number1+number2;
+          break;
+      case "MULTIPLICATION":
+          return number1*number2;
+          break;
+      case "SUBTRACTION":
+          return number1-number2;
+            break;
+      case "MODULO":
+        return number1%number2;
+      default:
+          return 0;
+  }
+  }
+
+
+  it('Operacion', function() {
+    var result = input(200,100,"DIVISION");
+    if(result!=0) {
+      expect(element(by.xpath("//h2[@class='ng-binding']")).getText()).not.toContain('NaN');
+      expect(element(by.xpath("//h2[@class='ng-binding']")).getText()).toEqual(result.toString());
+    }
+  });
+  
+});
